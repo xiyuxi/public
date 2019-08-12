03f3d561c142fe42358b21487145adc86f0acbe8
xiyuxi <xiyuxi@gmail.com>
2019-08-12 21:20:58 +0900

    Revert "fix: install.sh now works"
    This reverts commit 3e6da2e94aacf5ec8144d974c665d302d4253cc0.
    
    	modified:   dotfiles/install.sh

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -1,7 +1,25 @@
 #...
-DOTPATH=~/gitwk/public/dotfiles
+DOTPATH=~/public/dotfiles
+GITHUB_URL=https://github.com/xiyuxi/public.git
+# git が使えるなら git
+if has "git"; then
+    git clone --recursive "$GITHUB_URL" "$DOTPATH"
+# 使えない場合は curl か wget を使用する
+elif has "curl" || has "wget"; then
+    tarball="https://github.com/xiyuxi/public/dotfiles/archive/master.tar.gz"
+    # どっちかでダウンロードして，tar に流す
+    if has "curl"; then
+        curl -L "$tarball"
+    elif has "wget"; then
+        wget -O - "$tarball"
+    fi | tar zxv
+    # 解凍したら，DOTPATH に置く
+    mv -f dotfiles-master "$DOTPATH"
+else
+    die "curl or wget required"
+fi
 
-cd $DOTPATH
+cd ~/.dotfiles
 if [ $? -ne 0 ]; then
     die "not found: $DOTPATH"
 fi
